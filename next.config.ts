f533dd021b7a0791c6ba751221e6d87a4d9bd434
Ashar <syedashar09@gmail.com>
2025-01-18 07:36:26 +0530

    deploying static site

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,21 @@
 import type { NextConfig } from "next";
 
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+//   output: "export",
+//   basePath: "/portfolio",
+//   trailingSlash: true,
+// };
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
-  /* config options here */
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-  output: "export",
-  basePath: "/portfolio",
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? "/portfolio/" : "",
+  basePath: isProd ? "/portfolio" : "",
   trailingSlash: true,
+  output: "export",
 };
-
 export default nextConfig;
